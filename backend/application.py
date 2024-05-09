125f2dfd5b66c119203da611feabacbe6df99150
TsiarasKon <tsiaraskon@gmail.com>
2024-05-09 19:33:30 +0300

    BE: client_cache

--- backend/application.py
+++ backend/application.py
@@ -4,6 +4,16 @@ import os
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY")
+client_cache = {}
+
+
+def get_client(session_id, api_key=None):
+    if session_id not in client_cache:
+        # There can be a 1:N relation between key:sessions,
+        # but a key can't be changed in the same session
+        client_cache[session_id] = OpenAI(api_key=api_key)
+    return client_cache[session_id]
+
 
 @app.route('/')
 def home():
@@ -16,28 +26,28 @@ def chat():
     if not session_id:
         return jsonify({ 'response': 'Please provide your session_id.' })
 
-    if session_id not in session:
-        session[session_id] = []
+    api_key = request.json.get('api_key')
+    if not api_key and not session_id not in client_cache:
+        return jsonify({ 'response': 'Please provide an LLM API key.' })
 
     user_input = request.json.get('message')
     if not user_input:
         return jsonify({ 'response': 'Please provide an input.' })
-    session[session_id].append({ "role": "user", "content": user_input })
 
-    api_key = request.json.get('api_key')
-    if not api_key:
-        return jsonify({ 'response': 'Please provide an LLM API key.' })
+    if session_id not in session:
+        session[session_id] = [{ "role": "user", "content": user_input }]
 
     try:
-        client = OpenAI(api_key=api_key)
+        client = get_client(session_id, api_key)
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=session[session_id],
+            max_tokens=150,
             temperature=1
         )
         response_message = response.choices[0].message.content
         session[session_id].append({ "role": "system", "content": response_message })
-        return jsonify({ 'response': response_message, 'session_id': session_id })
+        return jsonify({ 'response': response_message })
     except Exception as e:
         return jsonify({ 'response': str(e) })
 
