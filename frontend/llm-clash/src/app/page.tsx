2104589087952ae454ff98b794294546386cfdf8
TsiarasKon <tsiaraskon@gmail.com>
2024-06-04 22:59:05 +0200

    FE: StyledLink

--- frontend/llm-clash/src/app/page.tsx
+++ frontend/llm-clash/src/app/page.tsx
@@ -1,17 +1,23 @@
+import StyledLink from '@/components/styled/StyledLink';
 import Link from 'next/link';
 import React from 'react';
 
 const Home: React.FC = () => {
     return (
         <div>
-            <Link href="/chat" className="block max-w-sm m-4 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <StyledLink href="/chat">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Chat</h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">It&apos;s just a ChatGPT wrapper, but with fewer features!</p>
-            </Link>
-            <Link href="/clash" className="block max-w-sm m-4 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <p className="font-normal text-gray-700 dark:text-gray-400">It&apos;s just an LLM wrapper, but with fewer features!</p>
+            </StyledLink>
+            <StyledLink href="/clash">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Clash</h5>
                 <p className="font-normal text-gray-700 dark:text-gray-400">Replace yourself with an LLM and see how it will conerse with another LLM after some inital prompting!</p>
-            </Link>
+            </StyledLink>
+            <StyledLink href="#" disabled={true}>
+                <span className="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">Coming Soon</span>
+                <h5 className="my-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Jailground</h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">Chat with a jailbroken version of the most popular LLMs!</p>
+            </StyledLink>
         </div>
     );
 }
---
+++ frontend/llm-clash/src/components/styled/StyledLink.tsx
@@ -0,0 +1,25 @@
+import Link, { LinkProps } from "next/link";
+import { ReactNode } from "react";
+
+interface IStyledLinkProps extends LinkProps {
+    children: ReactNode;
+    disabled?: boolean;
+    extraClasses?: string;
+}
+
+const StyledLink: React.FC<IStyledLinkProps> = ({ children, disabled = false, extraClasses = '', ...props }) => {
+    const classes = `block max-w-sm m-4 p-6 border rounded-lg shadow bg-gray-800 border-gray-700 ${extraClasses} ${disabled
+        ? "opacity-50 cursor-default"
+        : "hover:bg-gray-700"}`;
+
+    return (
+        <Link
+            className={classes}
+            {...props}
+        >
+            {children}
+        </Link>
+    );
+};
+
+export default StyledLink;
