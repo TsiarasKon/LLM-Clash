a13e89da2931efaffb29084b8605f9e38a1e7be7
TsiarasKon <tsiaraskon@gmail.com>
2024-06-05 23:55:07 +0200

    FE: LoadingSpinner component

--- frontend/llm-clash/src/app/page.tsx
+++ frontend/llm-clash/src/app/page.tsx
@@ -1,5 +1,4 @@
 import StyledLink from '@/components/styled/StyledLink';
-import Link from 'next/link';
 import React from 'react';
 
 const Home: React.FC = () => {
@@ -11,7 +10,7 @@ const Home: React.FC = () => {
             </StyledLink>
             <StyledLink href="/clash">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Clash</h5>
-                <p className="font-normal text-gray-700 dark:text-gray-400">Replace yourself with an LLM and see how it will conerse with another LLM after some inital prompting!</p>
+                <p className="font-normal text-gray-700 dark:text-gray-400">Set up 2 LLMs against each other and, after some initial prompting, let their conversation run wild!</p>
             </StyledLink>
             <StyledLink href="#" disabled={true}>
                 <span className="bg-yellow-100 text-yellow-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">Coming Soon</span>
